620f3c2282ec1ae334f32dbf362dbe43437ea573
Miguel Reis <guelsouzar@gmail.com>
2023-04-22 16:47:38 -0300

    feat: add class

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,7 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath } = movie;
     return (
-      <div>
+      <div className='movie-card'>
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
